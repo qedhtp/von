660589a426738ba065c3ff9d2374484a7e25ae67
ttmachine <tianxink6@gmail.com>
2024-10-04 15:03:25 +0800

    changed

--- wthis.go
+++ wthis.go
@@ -10,27 +10,24 @@ import (
 	"github.com/PuerkitoBio/goquery"
 )
 
-func getPronounce (doc *goquery.Document) {
 
+
+func getTranslate (doc *goquery.Document) {
+
+	// pronounce
 	pronounce_element := doc.Find("div.per-phone > span.phonetic")
-
 	English := pronounce_element.First().Text()
 	American := pronounce_element.Last().Text()
 
-	
-
-	if pronounce_element.Length() > 0 {
-		fmt.Printf("\n    英: %s    ", English)
-		fmt.Printf("美: %s    \n", American)
-	}
-
-}
-
-func getTranslate (doc *goquery.Document) {
 	// translate a word
-	translate_element := doc.Find("ul.basic > li.word-exp")
-	word_form := doc.Find("ul.word-wfs-less > li.word-wfs-cell-less")
-	phrase := doc.Find("div.col2 > a.point")
+	word_element := doc.Find("ul.basic > li.word-exp")
+	word_form_ch := doc.Find("ul.word-wfs-less > li.word-wfs-cell-less > p.grey")
+	word_form_en := doc.Find("ul.word-wfs-less > li.word-wfs-cell-less > span.transformation")
+	phrase_en := doc.Find("div.webPhrase > ul > li > div.col2 > a.point")
+	phrase_ch := doc.Find("div.webPhrase > ul > li > div.col2 > p.sen-phrase")
+
+	// example
+	
 	
 	// translate a sentence
 	translate_content := doc.Find("p.trans-content")
@@ -38,24 +35,42 @@ func getTranslate (doc *goquery.Document) {
 	
 
 	// Determine if a word or a sentence
-	if translate_element.Length() > 0 {
-		for i := 0; i < translate_element.Length(); i++ {
-			text := translate_element.Eq(i).Text()
+	if word_element.Length() > 0 {
+
+		// pronoounce
+		fmt.Printf("\n    --------------发音---------------")
+		if pronounce_element.Length() > 0 {
+			fmt.Printf("\n    英: %s    ", English)
+			fmt.Printf("美: %s    \n", American)
+		}
+
+		// word meaning
+		fmt.Printf("\n    --------------释义---------------")
+		for i := 0; i < word_element.Length(); i++ {
+			text := word_element.Eq(i).Text()
 			fmt.Printf("\n    %s", text)
 		}
 		fmt.Printf("\n\n")
-	
-		for i :=0; i < word_form.Length(); i++ {
-			text := word_form.Eq(i).Text()
-			fmt.Printf("    %s", text)
+		
+		// word form
+		fmt.Printf("    ----------------形态----------------\n")
+		for i :=0; i < word_form_ch.Length(); i++ {
+			text_ch := word_form_ch.Eq(i).Text()
+			text_en := word_form_en.Eq(i).Text()
+			
+			fmt.Printf("    %s", text_ch)
+			fmt.Printf(": %s", text_en)
 		}
 		fmt.Printf("\n\n")
 	
-		for i := 0; i < phrase.Length(); i++ {
-			text := phrase.Eq(i).Text()
-			fmt.Printf("    %s", text)
+		// phrase
+		fmt.Printf("    ----------------短语----------------\n")
+		for i := 0; i < phrase_en.Length(); i++ {
+			text_en := phrase_en.Eq(i).Text()
+			text_ch := phrase_ch.Eq(i).Text()
+			fmt.Printf("    %s", text_en)
+			fmt.Printf("  %s\n", text_ch)
 		}
-		fmt.Printf("\n")
 
 
 	} else{
@@ -94,7 +109,6 @@ func main() {
 		}
 
 		// Call a function with *goquery.document
-		getPronounce(doc)
 		getTranslate(doc)
 
 
