13b1a79f113bffc2c9e6b8697c0a9f4eb516448d
qedhtp <tianxinke6@gmail.com>
2024-10-05 12:56:22 +0800

    changed

--- von.go
+++ von.go
@@ -25,6 +25,8 @@ func getTranslate (doc *goquery.Document) {
 	word_form_en := doc.Find("ul.word-wfs-less > li.word-wfs-cell-less > span.transformation")
 	phrase_en := doc.Find("div.webPhrase > ul > li > div.col2 > a.point")
 	phrase_ch := doc.Find("div.webPhrase > ul > li > div.col2 > p.sen-phrase")
+	example_en := doc.Find("div.col2 > div.word-exp > div.sen-eng")
+	example_ch := doc.Find("div.col2 > div.word-exp > div.sen-ch")
 
 	// example
 	
@@ -32,7 +34,6 @@ func getTranslate (doc *goquery.Document) {
 	// translate a sentence
 	translate_content := doc.Find("p.trans-content")
 
-	
 
 	// Determine if a word or a sentence   
 	if word_element.Length() > 0 {
@@ -50,26 +51,39 @@ func getTranslate (doc *goquery.Document) {
 			text := word_element.Eq(i).Text()
 			fmt.Printf("\n    %s", text)
 		}
-		fmt.Printf("\n\n")
+		fmt.Printf("\n")
 		
 		// word form
-		fmt.Printf("    ----------------形态----------------\n")
-		for i :=0; i < word_form_ch.Length(); i++ {
-			text_ch := word_form_ch.Eq(i).Text()
-			text_en := word_form_en.Eq(i).Text()
-			
-			fmt.Printf("    %s", text_ch)
-			fmt.Printf(": %s", text_en)
+		if word_form_ch.Length() > 1 {
+			fmt.Printf("\n    --------------形态---------------\n")
+			for i :=0; i < word_form_ch.Length(); i++ {
+				text_ch := word_form_ch.Eq(i).Text()
+				text_en := word_form_en.Eq(i).Text()
+				
+				fmt.Printf("    %s", text_ch)
+				fmt.Printf(": %s", text_en)
+			}
+			fmt.Printf("\n")
 		}
-		fmt.Printf("\n\n")
+
 	
 		// phrase
-		fmt.Printf("    ----------------短语----------------\n")
+		fmt.Printf("\n    --------------短语----------------\n")
 		for i := 0; i < phrase_en.Length(); i++ {
 			text_en := phrase_en.Eq(i).Text()
 			text_ch := phrase_ch.Eq(i).Text()
-			fmt.Printf("    %s", text_en)
+			fmt.Printf("    %d.%s", i+1,text_en)
 			fmt.Printf("  %s\n", text_ch)
+		}
+		fmt.Printf("\n")
+
+		// example
+		fmt.Printf("    --------------例句---------------\n")
+		for i :=0; i < example_en.Length(); i++ {
+			text_en := example_en.Eq(i).Text()
+			text_ch := example_ch.Eq(i).Text()
+			fmt.Printf("    %d.%s\n", i+1, text_en)
+			fmt.Printf("      %s\n", text_ch)
 		}
 
 
